909c51d3352db8e5b887aeef9bff6a286049fbed
Muhammed Hayder <muhammed.h.m287@gmail.com>
2018-06-14 12:05:53 +0300

    Add files via upload

---
+++ rx_code.ino
@@ -0,0 +1,218 @@
+const int motA_pin1=4;
+const int motA_pin2=5;
+const int motA_EN=3; //pwm
+
+const int motB_pin1=13;
+const int motB_pin2=12;
+const int motB_EN=11; //pwm
+char ss=220;
+char s=150;
+
+
+#include <VirtualWire.h>
+
+void setup() {
+
+vw_set_rx_pin(8);
+vw_set_ptt_inverted(true);
+vw_setup(2000);
+vw_rx_start();
+
+motor_init();
+Serial.begin(9600);
+}
+
+
+void loop() {
+int ton=5;
+int tres=1;
+  
+uint8_t buf[VW_MAX_MESSAGE_LEN];
+uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+if (vw_get_message(buf, &buflen)) { Serial.println(buf[0]);
+   switch ( buf[0]) {
+    case 56 : 
+    forward() ;
+    break;
+    case 57 : 
+    forward1() ;
+    break;
+    case 52 : 
+    left() ;
+    break;
+    case 54 : 
+    right() ;
+    break;
+    case 55 : 
+    right1();
+    break;
+    case 51 : 
+    left1();
+    break;
+    case 53 : 
+    right1();
+    break;
+    case 49 : 
+    revers1();
+    break;
+    case  50: 
+    revers();
+    break;
+    default :
+    stop();
+    break; 
+   }
+}
+}
+
+void motor_init(){
+pinMode (motA_pin1,OUTPUT);
+pinMode (motA_pin2,OUTPUT);
+
+pinMode (motB_pin1,OUTPUT);
+pinMode (motB_pin2,OUTPUT);
+
+pinMode (motA_EN,OUTPUT);
+pinMode (motB_EN,OUTPUT);
+
+analogWrite(motA_EN,0);
+digitalWrite(motA_pin1,LOW);
+digitalWrite(motA_pin2,LOW);
+
+analogWrite(motB_EN,0);
+digitalWrite(motB_pin1,LOW);
+digitalWrite(motB_pin2,LOW);
+}
+
+void left(){
+
+Serial.println("left");
+analogWrite(motA_EN,50); 
+analogWrite(motB_EN,250); 
+digitalWrite(motA_pin1,LOW);
+digitalWrite(motA_pin2,HIGH);
+
+digitalWrite(motB_pin1,HIGH);
+digitalWrite(motB_pin2,LOW);
+
+}
+
+ 
+
+void right(){
+Serial.println("right");
+analogWrite(motA_EN,250);
+analogWrite(motB_EN,50);
+
+digitalWrite(motA_pin1,LOW);
+digitalWrite(motA_pin2,HIGH);
+
+digitalWrite(motB_pin1,HIGH);
+digitalWrite(motB_pin2,LOW);
+
+}
+
+void forward(){
+
+Serial.println("for");
+analogWrite(motA_EN,s);
+digitalWrite(motA_pin1,LOW); 
+digitalWrite(motA_pin2,HIGH); 
+
+// analogWrite(motB_EN,speed-10);
+analogWrite(motB_EN,s);
+digitalWrite(motB_pin1,HIGH); 
+digitalWrite(motB_pin2,LOW); 
+
+ 
+
+}
+
+void revers(){
+
+Serial.println("revers");
+
+analogWrite(motA_EN,s);
+digitalWrite(motA_pin1,HIGH);
+digitalWrite(motA_pin2,LOW);
+
+analogWrite(motB_EN,s);
+digitalWrite(motB_pin1,LOW); 
+digitalWrite(motB_pin2,HIGH);
+
+}
+
+ 
+
+void stop(){
+Serial.println("stop");
+analogWrite(motA_EN,0);
+digitalWrite(motA_pin1,LOW);
+digitalWrite(motA_pin2,LOW);
+
+analogWrite(motB_EN,0);
+digitalWrite(motB_pin1,LOW); 
+digitalWrite(motB_pin2,LOW);
+
+}
+
+
+void left1(){
+
+analogWrite(motA_EN,50); 
+analogWrite(motB_EN,250); 
+digitalWrite(motA_pin1,LOW);
+digitalWrite(motA_pin2,HIGH);
+
+digitalWrite(motB_pin1,HIGH);
+digitalWrite(motB_pin2,LOW);
+
+}
+
+ 
+
+void right1(){
+Serial.println("right1");
+analogWrite(motA_EN,250);
+analogWrite(motB_EN,50);
+
+digitalWrite(motA_pin1,LOW);
+digitalWrite(motA_pin2,HIGH);
+
+digitalWrite(motB_pin1,HIGH);
+digitalWrite(motB_pin2,LOW);
+
+}
+
+void forward1(){
+
+
+analogWrite(motA_EN,ss);
+digitalWrite(motA_pin1,LOW); 
+digitalWrite(motA_pin2,HIGH); 
+
+analogWrite(motB_EN,ss);
+digitalWrite(motB_pin1,HIGH); 
+digitalWrite(motB_pin2,LOW);
+
+ 
+
+}
+
+void revers1(){
+
+Serial.println("revers1");
+analogWrite(motA_EN,ss);
+digitalWrite(motA_pin1,HIGH);
+digitalWrite(motA_pin2,LOW);
+
+analogWrite(motB_EN,ss);
+digitalWrite(motB_pin1,LOW); 
+digitalWrite(motB_pin2,HIGH);
+
+}
+
+
+
+
---
+++ tx_code.ino
@@ -0,0 +1,119 @@
+#include<Wire.h>
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ;
+
+#include <VirtualWire.h>
+char *controller;
+int t = 5;
+void setup() {
+  
+ 
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Serial.begin(9600);    // 9600
+
+  vw_set_ptt_inverted(true); //
+  vw_set_tx_pin(10);
+  vw_setup(2000);// speed of data transfer Kbps
+
+}
+
+void loop() {
+Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.println(AcZ);
+
+
+  if ( AcX < -6000  && AcX > -10000 ) 
+  {
+      Serial.println("send 8");
+      controller="8";
+      vw_send((uint8_t *)controller, strlen(controller));
+      vw_wait_tx();
+      
+      //delay(t);
+  }
+  else if (AcX < -10000  && AcX > -18000  ) 
+  {   
+      Serial.println("send 9");
+      controller="9";
+      vw_send((uint8_t *)controller, strlen(controller));
+      vw_wait_tx();
+      delay(t);
+  }
+  
+  else if ( AcX > 6000  && AcX < 10000 ) 
+  {   
+      Serial.println("send 2");
+      controller="2";
+      vw_send((uint8_t *)controller, strlen(controller));
+      vw_wait_tx();
+      delay(t);
+  }
+
+  else if ( AcX > 10000  && AcX < 18000 ) 
+  {   
+      Serial.println("send 1");
+      controller="1";
+      vw_send((uint8_t *)controller, strlen(controller));
+      vw_wait_tx();
+      delay(t);
+  }
+    
+  else if ( AcY > 6000  && AcY < 10000 ) 
+  {
+      Serial.println("send 6");
+      controller="6";
+      vw_send((uint8_t *)controller, strlen(controller));
+      vw_wait_tx();
+      delay(t);
+  }
+   else if ( AcY > 10000  && AcY < 18000 ) 
+  {
+      Serial.println("send 7");
+      controller="7";
+      vw_send((uint8_t *)controller, strlen(controller));
+      vw_wait_tx();
+      delay(t);
+  }
+  
+  else if ( AcY < -6000  && AcY > -10000 ) 
+  {
+      Serial.println("send 4");
+      controller="4";
+      vw_send((uint8_t *)controller, strlen(controller));
+      vw_wait_tx();
+      delay(t);
+  }
+  else if ( AcY < -10000  && AcY > -18000 ) 
+  {
+      Serial.println("send 3");
+      controller="3";
+      vw_send((uint8_t *)controller, strlen(controller));
+      vw_wait_tx();
+      delay(t);
+  }
+  else {
+      Serial.println("send 0");
+      controller="0";
+      vw_send((uint8_t *)controller, strlen(controller));
+      vw_wait_tx();
+      delay(t);
+  }
+
+ 
+  
+}
+
+
